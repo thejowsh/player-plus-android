bf7f57f003d551286d48fbe3aaaff591c11b7485
Josh <josh@eightbitninjas.com>
2022-02-13 00:22:57 +0800

    Registering Sensor events and converting data into rotation in degress

--- app/src/main/java/com/overplay/test/playerplus/MainActivity.kt
+++ app/src/main/java/com/overplay/test/playerplus/MainActivity.kt
@@ -1,25 +1,34 @@
 package com.overplay.test.playerplus
 
+import android.hardware.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Half.EPSILON
+import android.util.Log
 import androidx.core.view.WindowCompat
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
 import com.overplay.test.playerplus.databinding.ActivityMainBinding
+import java.util.*
+import kotlin.math.cos
+import kotlin.math.sin
+import kotlin.math.sqrt
 
 /**
  * Created by Josh Basarte 2022-02-12
  * Main Activity containing the Media Player object.
  */
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), Orientation.Listener {
 
+    /** UI Binding object */
+    private lateinit var binding: ActivityMainBinding
+    /** Exoplayer instance */
     private val player by lazy(LazyThreadSafetyMode.NONE) {
         ExoPlayer.Builder(MainActivity@this)
             .build()
     }
-
-    private lateinit var binding: ActivityMainBinding
-
+    private lateinit var mSensorManager : SensorManager
+    private lateinit var mOrientation: Orientation
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +36,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.videoView.player = player
+        mSensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        mOrientation = Orientation(this)
     }
 
     override fun onResume() {
@@ -37,7 +48,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
-        initializePlayer()
+        initializePlayer() // start playing video
+        mOrientation.startListening(this) // listen for pitch/yaw events
     }
 
     private fun initializePlayer() {
@@ -46,12 +58,17 @@ class MainActivity : AppCompatActivity() {
                     "sample/WeAreGoingOnBullrun.mp4") //  TODO: Load this from strings or something.
 
         player.setMediaItem(mediaItem)
-        player.playWhenReady = true // TODO: Per requirement, call play() after 4s after media is loaded
+        player.playWhenReady = true // TODO: Per requirement, call play() after 4s after media is load
     }
 
 
     override fun onStop() {
         super.onStop()
         player.release()
+        mOrientation.stopListening()
+    }
+
+    override fun onOrientationChanged(pitch: Float, roll: Float) {
+        Log.d("MAIN", "pitch:$pitch, roll:$roll")
     }
 }
